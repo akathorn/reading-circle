fb679c5eb6dc5001cd56f8e3775719ded913fe0f
Daniel Espino <akathorn@gmail.com>
2021-09-13 20:40:22 +0200

    Improve tests

---
+++ itertools/itertools_test.py
@@ -0,0 +1,79 @@
+import pytest
+import time
+
+import mitertools
+import itertools
+
+# Infinite iterators. Iterators for which next() is very slow.
+
+
+class BaseTest:
+    f = ""
+
+    def call(self, *args, **kwargs):
+        result = getattr(mitertools, self.f)(*args, **kwargs)
+        expected = getattr(itertools, self.f)(*args, **kwargs)
+        return result, expected
+
+    def compare(self, a, b, n=None):
+        """Compare the first n elements of iterables a and b."""
+        slice = itertools.islice(zip(a, b), n)
+        assert all([x == y for x, y in slice])
+
+
+class TestCombinations(BaseTest):
+    f = "combinations"
+
+    def test_basic(self):
+        result, expected = self.call("abc", 2)
+        self.compare(result, expected)
+
+    def test_big(self):
+        result, expected = self.call(range(100), 3)
+        self.compare(result, expected)
+
+    def test_bigger(self):
+        result, expected = self.call(range(100000000), 2)
+        self.compare(result, expected, 100)
+
+
+class TestPermutations(BaseTest):
+    f = "permutations"
+
+    def test_basic(self):
+        result, expected = self.call("abc", 2)
+        self.compare(result, expected)
+
+    def test_big(self):
+        result, expected = self.call(range(100), 3)
+        self.compare(result, expected)
+
+    def test_bigger(self):
+        result, expected = self.call(range(100000000))
+        self.compare(result, expected, 100)
+
+
+class TestProduct(BaseTest):
+    f = "product"
+
+    def test_basic(self):
+        result, expected = self.call([1, 2], ["a", "b"])
+        self.compare(result, expected)
+
+    def test_multiple(self):
+        result, expected = self.call(
+            [1, 2], ["a", "b"], [True, False], [None, None, None]
+        )
+        self.compare(result, expected)
+
+    def test_repeat(self):
+        result, expected = self.call("ab", "cd", repeat=4)
+        self.compare(result, expected)
+
+    def test_big(self):
+        result, expected = self.call(range(10000), range(1000))
+        self.compare(result, expected)
+
+    def test_bigger(self):
+        result, expected = self.call(range(1000000000), range(1000000000))
+        self.compare(result, expected, 100)
--- itertools/mitertools.py
+++ itertools/mitertools.py
@@ -1,6 +1,36 @@
 import itertools
 
 
+def combinations(iterable, length=2):
+    """combinations('ABCD', 2) --> AB AC AD BC BD CD"""
+    elements = list(iterable)
+    indices = list(reversed(range(len(elements))))
+
+    stack = [[i] for i in indices]
+    while stack:
+        current = stack.pop()
+        if len(current) == length:
+            yield tuple(elements[i] for i in current)
+        else:
+            stack.extend(
+                current + [i] for i in indices if i not in current and i > current[-1]
+            )
+
+
+def permutations(iterable, length=2):
+    """permutations('ABCD', 2) --> AB AC AD BA BC BD CA CB CD DA DB DC"""
+    elements = list(iterable)
+    indices = list(reversed(range(len(elements))))
+
+    stack = [[i] for i in indices]
+    while stack:
+        current = stack.pop()
+        if len(current) == length:
+            yield tuple(elements[i] for i in current)
+        else:
+            stack.extend(current + [i] for i in indices if i not in current)
+
+
 def product(*iterables, repeat=1):
     """product('ABCD', repeat=2) --> AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD"""
     iterables = list(iterables) * repeat
@@ -27,33 +57,3 @@ def product(*iterables, repeat=1):
         else:
             used.append(iterables[-1])
             stack.append(iter(iterables.pop()))
-
-
-def permutations(iterable, length=2):
-    """permutations('ABCD', 2) --> AB AC AD BA BC BD CA CB CD DA DB DC"""
-    elements = list(iterable)
-    indices = list(reversed(range(len(elements))))
-
-    stack = [[i] for i in indices]
-    while stack:
-        current = stack.pop()
-        if len(current) == length:
-            yield tuple(elements[i] for i in current)
-        else:
-            stack.extend(current + [i] for i in indices if i not in current)
-
-
-def combinations(iterable, length=2):
-    """combinations('ABCD', 2) --> AB AC AD BC BD CD"""
-    elements = list(iterable)
-    indices = list(reversed(range(len(elements))))
-
-    stack = [[i] for i in indices]
-    while stack:
-        current = stack.pop()
-        if len(current) == length:
-            yield tuple(elements[i] for i in current)
-        else:
-            stack.extend(
-                current + [i] for i in indices if i not in current and i > current[-1]
-            )
